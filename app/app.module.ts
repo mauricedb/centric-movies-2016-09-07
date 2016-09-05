f803acb97035e009341366b630b5e62ac7ea6daa
Maurice de Beijer <maurice.de.beijer@gmail.com>
2016-09-05 11:23:21 +0200

    Working app

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { HttpModule }    from '@angular/http';
 
 import { AppComponent }  from './app.component';
@@ -9,6 +10,7 @@ import { MovieComponent } from './movie/movie.component';
 @NgModule({
   imports: [ 
     BrowserModule,
+    FormsModule,
     HttpModule 
   ],
   declarations: [ 
